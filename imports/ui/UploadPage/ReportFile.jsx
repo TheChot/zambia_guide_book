68fbe6365717d46db8e3a19730b2b1512454de11
theChot <chotxhm@gmail.com>
2018-09-20 23:15:05 +0200

    Upload Page Components added

---
+++ imports/ui/UploadPage/ReportFile.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class ReportFile extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Report</h1>
+                
+            </div>
+        )
+    }
+
+}
+
+export default ReportFile;
