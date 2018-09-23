b062fcb8acc458fea5dab7d7d9b750fca7ec341e
theChot <chotxhm@gmail.com>
2018-09-23 23:26:38 +0200

    working on map cordinates

--- imports/ui/HomePage/Map.jsx
+++ imports/ui/HomePage/Map.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+//import Zambia from './';
 
 class Map extends Component{
 
@@ -6,7 +7,10 @@ class Map extends Component{
         return(
             <div>
                 <h1>Map</h1>
-                
+                <img src="mapofZambia.jpg" alt="" className="theMap" useMap="ZambiaMap"/>
+                <map name="ZambiaMap">
+                    <area shape="poly" coords="262,306, 352,282, 372,328, 270,351" href="/MainBusinessPage" alt=""/>
+                </map>
             </div>
         )
     }
