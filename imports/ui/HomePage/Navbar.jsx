9df8b96c408874914f26240e4aedccfa20c130cc
theChot <chotxhm@gmail.com>
2018-09-23 22:05:14 +0200

    created a navbar

--- imports/ui/HomePage/Navbar.jsx
+++ imports/ui/HomePage/Navbar.jsx
@@ -5,7 +5,28 @@ class Navbar extends Component{
     render(){
         return(
             <div>
-                <h1>NavBar</h1>
+                <nav className="navbar navbar-expand-md navbar-light">
+                <div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/MainCityPage">City</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/MainBusinessPage">Business</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/Login">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/SignUp">Sign-Up</a>
+                        </li>
+                    </ul>
+                </div>
+
+                </nav>
 
             </div>
         )
