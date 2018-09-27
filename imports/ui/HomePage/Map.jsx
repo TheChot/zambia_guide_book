8c870a003de6683568791a5f6c54ece2cd7924b3
theChot <chotxhm@gmail.com>
2018-09-27 18:48:04 +0200

    removed map

--- imports/ui/HomePage/Map.jsx
+++ imports/ui/HomePage/Map.jsx
@@ -7,39 +7,7 @@ class Map extends Component{
         return(
             <div>
                 <h1>Map</h1>
-                <div>
-                    <img src="mapofZambia.jpg" alt="" className="theMap" useMap="ZambiaMap"/>
-                    <div className="Lusaka">
-                        <a href="/MainCityPage"><img src="star.png" alt="" className="cityIcon"/></a>                        
-                    </div>
-                    <div className="Central">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>                        
-                    </div>
-                    <div className="Copperbelt">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Southern">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Western">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="NorthWestern">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Eastern">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Southern">
-                       <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Northern">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                    <div className="Luapula">
-                        <a href="/MainCityPage"><img src="city.png" alt="" className="cityIcon"/></a>
-                    </div>
-                </div>
+                
                
                 
             </div>
