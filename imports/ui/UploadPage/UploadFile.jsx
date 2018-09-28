3e3f046472b3adbdf0dfc3a2cefeb14d976cfb55
theChot <chotxhm@gmail.com>
2018-09-28 22:45:16 +0200

    upload page designed

--- imports/ui/UploadPage/UploadFile.jsx
+++ imports/ui/UploadPage/UploadFile.jsx
@@ -8,7 +8,21 @@ class UploadFile extends Component{
         return(
             <div>
                 <h1>Upload</h1>
-                <form action=""></form>
+                <form action="">
+                    <label htmlFor="">Business Name</label>
+                    <input name="BusinessName" type="text"/>
+                    <label htmlFor="">Phone Number 1</label>
+                    <input name="PhoneNumber1" type="text"/>
+                    <label htmlFor="">Phone Number 2</label>
+                    <input name="PhoneNumber2" type="text"/>
+                    <label htmlFor="">Email</label>
+                    <input name="Email" type="text"/>
+                    <label htmlFor="">Location</label>
+                    <input name="Location" type="text"/>
+                    <label htmlFor="">Service</label>
+                    <input name="Service" type="text"/>
+                    <button type="submit">Enter Data</button>
+                </form>
                 
             </div>
         )
