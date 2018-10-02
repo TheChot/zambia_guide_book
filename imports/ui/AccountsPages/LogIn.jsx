61f6b54734c78520a56fa892f16d40b35a208244
theChot <chotxhm@gmail.com>
2018-10-02 13:22:54 +0200

    added login function and log out function

--- imports/ui/AccountsPages/LogIn.jsx
+++ imports/ui/AccountsPages/LogIn.jsx
@@ -1,12 +1,37 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {Meteor} from 'meteor/meteor';
+import {Accounts} from 'meteor/accounts-base';
 
 class Login extends Component{
+
+    loginUser = e =>{
+        e.preventDefault();
+
+        const {target} = e;
+        const password = target.password.value;
+        const email = target.username.value;
+
+        Meteor.loginWithPassword(email, password,(err)=>{
+            if(err){
+                console.log(err.reason);
+            } else{
+                FlowRouter.go('/UserPage');
+            }
+        })
+    }
 
     render(){
         return(
             <div>
                 <h1>Login</h1>
+                <form action="" onSubmit ={this.loginUser}>
+                    <label htmlFor="">UserName:</label>
+                    <input type="text" name="username" id="username" required/><br/><br/>
+                    <label htmlFor="">Password:</label>
+                    <input type="password" name="" id="password" required/><br/><br/>
+                    <button type="submit">Enter</button>
                 
+                </form><br/>
             </div>
         )
     }
