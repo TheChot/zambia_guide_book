02a59e935a8092ac25d3178acae13befa8071cd3
theChot <chotxhm@gmail.com>
2018-10-11 21:07:58 +0200

    modified navbar and added logo to files

--- imports/ui/HomePage/Map.jsx
+++ imports/ui/HomePage/Map.jsx
@@ -42,7 +42,7 @@ class Map extends Component{
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                     </a>
@@ -111,7 +111,7 @@ class Map extends Component{
                 
             </div>
         )
-    }className="btn peach-gradient btn-rounded "
+    }
 
 }
 
--- imports/ui/HomePage/Navbar.jsx
+++ imports/ui/HomePage/Navbar.jsx
@@ -10,7 +10,9 @@ class Navbar extends Component{
 
                    <nav className="navbar navbar-expand-md navbar-light bg-success">
                         <div className= "container-fluid navigation">
-                            <a className= "navbar-brand" href="/"><img src="#" alt="LogoHere"/></a>
+                            <a className= "navbar-brand" href="/">
+                                <img src="logo/bird.png" alt=""/>
+                            </a>
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
@@ -46,22 +48,25 @@ class Navbar extends Component{
                             <div className="collapse navbar-collapse" id="navbarResponsive">
                                 <ul className="navbar-nav loggedInNav">
                                     <li className="nav-item">
-                                        <a href="/">Home</a>
+                                        <a className="nav-link" href="/">Home</a>
+                                    </li>
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Upload
+                                        </a>
+                                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a className="dropdown-item" href="/UploadFile">Upload A Picture</a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="/UploadBusiness">Add a Business</a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="/UploadPlace">Add an Attraction</a>
+                                        </div>
                                     </li>
                                     <li className="nav-item">
-                                        <a href="/UploadFile">Upload A Picture</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="/UploadBusiness">Add a Business</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="/UploadPlace">Add an Attraction</a>
-                                    </li> 
-                                    <li className="nav-item">
-                                        <a href="/UserPage">Profile</a>
+                                        <a className="nav-link" href="/UserPage">Profile</a>
                                     </li>                              
                                     <li className="nav-item">
-                                        <a href="#">Logout</a>
+                                        <a className="nav-link" href="#">Logout</a>
                                     </li>                                
                                 </ul>
                             </div>
